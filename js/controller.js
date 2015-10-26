a819dd7b4ef626280d853a9ea875d38fe237e4dd
Joep Schyns <joepschyns@gmail.com>
2015-10-26 23:18:48 +0100

    Mega update
    borders changing
    fonts engine

--- js/controller.js
+++ js/controller.js
@@ -2,38 +2,147 @@
 TODO
 laad ding maken voor pagina
 
+save as optimized svg
+convert flowroot text to "normal text" (not supported by html 5) text>unflow
 **/
 
 var app = angular.module('myApp', ['ngMaterial']);
-app.controller('appController', function($scope) {
-$scope.textElements = [];
-$scope.colorElements = [];
-$scope.temp = "test";
-$scope.color = {
-	red:223,
-	green:250,
-	blue:255
+app.controller('appController', function($scope, $mdDialog) {
+	var alert;
+	$scope.textElements = [];
+	$scope.colorElements = [];
+	$scope.temp = "test";
+	$scope.color = {
+		red:223,
+		green:250,
+		blue:255
+	}
+	$scope.logo;
+	var textElements = "text";
+	function getData(url)
+{
+    return $.getJSON("http://query.yahooapis.com/v1/public/yql?"+
+      "q=select%20*%20from%20html%20where%20url%3D%22"+
+      encodeURIComponent(url)+"%22&format=json'&callback=?");
+      // , function(data){
+      //     if(data.results[0]){
+      //         $scope.missingFontWebsites[name].push(url);
+      //     }else{
+      //     	console.debug("nee")
+      //     }  
+     // }
+    //);
 }
-$scope.logo;
-var coloredElements = ["fill"]; //totdo get all style elements that color
-	 angular.element(document).ready(function () {
-		$("text" ).each(function(i,ob) {
-			$scope.textElements.push({text:this.textContent});
-				//this.setAttribute("ng-bind","textElements[" + i +"].text");
+	$scope.missingFontWebsites = [];
+	var fontWebsites = [{before:"http://www.1001fonts.com/",after:"-font.html"},{before:"https://typekit.com/fonts/",after:""},{before:"http://fontzone.net/font-details/",after:""},{before:"https://www.google.com/fonts/specimen/",after:""}]; //{before:"",after:""}
+	var fontSitesCallBackData = [];
+	var fontSitesCallBack = [];
+	function getFontSitesCallBack(name){
+		var wait = [];
+		var waitData = [];
+		$.each(fontWebsites,function(i,ob){
+			var url = ob.before + name + ob.after;
+			wait.push(getData(url));
+			waitData.push({name:name,url:url});
 		});
-		var colorElements = $.merge($("path"),$("tspan"));//add elements with color fill //TODO loop trough nested svg elements
-		colorElements.each(function(i,ob) {
-			var rawColor = $(ob).css('fill').match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-			var color = {css:rawColor[0],r:rawColor[1],g:rawColor[2],b:rawColor[3]};
-			for (var i = 0; i < $scope.colorElements.length; i++) {
-				if($scope.colorElements[i].color.css == color.css){
-					$scope.colorElements[i].objects.push(ob);
-					return true; //continue each
+		$.merge(fontSitesCallBack,wait);//merge callbacks
+		$.merge(fontSitesCallBackData,waitData);//merge callbacks
+	}
+	var coloredElements = ["fill","stroke"]; //totdo get all style elements that color
+	function showDialog($event) {
+
+		var parentEl = angular.element(document.body);
+		$mdDialog.show({
+			parent: parentEl,
+			targetEvent: $event,
+			templateUrl:'templates/font-dialog-template.html',
+			locals: {
+				notAvailableFonts: $scope.notAvailableFonts,
+				missingFontWebsites: $scope.missingFontWebsites
+			},
+				controller: $scope.dialogController
+			});
+
+	}
+	$scope.dialogController = function DialogController($scope, $mdDialog, notAvailableFonts,missingFontWebsites) {
+			$scope.notAvailableFonts = notAvailableFonts;
+			$scope.missingFontWebsites = missingFontWebsites;
+			$scope.closeDialog = function() {
+				$mdDialog.hide();
+			}
+			$scope.update = function(){
+				console.debug("test");
+			}
+
+		}
+	$scope.notAvailableFonts = [];
+	 angular.element(document).ready(function () {
+		var fontDetector = new Detector();
+
+		$(textElements).each(function(i,ob) { //get all texts
+			var font = $(ob).css('font-family').toLowerCase();
+			if(fontDetector.detect(font) == false){ //detecr if font is available
+				$.each($scope.notAvailableFonts,function(i,ob){ //check if font is not in list
+					if(ob == font.toLowerCase())
+						return false; //break each
+				});
+				var name= font.substring(0,1).toUpperCase() + font.substring(1)
+				$scope.notAvailableFonts.push(name); //add font to list
+				getFontSitesCallBack(name);
+			}
+			
+			var text = this.textContent;
+			for (var i = 0; i < $scope.textElements.length; i++) { //not add fields for equal texts
+					if($scope.textElements[i].text == text){
+						$scope.textElements[i].objects.push(ob);
+						return true; //continue each
+					}
+				};
+				var objects = [];
+				objects.push(ob);
+				$scope.textElements.push({text:text,objects:objects});//create new for new color	
+				
+		});
+		if($scope.notAvailableFonts.length > 0){ //show dialog if one or more fonts are not available
+			var obj = []
+			$.when.apply($, fontSitesCallBack).done(function(){
+				 var l = arguments.length,
+        				i,
+        				jqxhr;
+    			for(var i = 0; i < l; i++){
+    				if(arguments[i][0].results.length > 0){
+    					console.debug(fontSitesCallBackData[i]);
+    					if($scope.missingFontWebsites[fontSitesCallBackData[i].name] == undefined){
+    						var ob = [];
+    						ob.push(fontSitesCallBackData[i].url);
+    						$scope.missingFontWebsites[fontSitesCallBackData[i].name] = ob;
+
+    					}else{
+    						console.debug("sdf");
+    					$scope.missingFontWebsites[fontSitesCallBackData[i].name].push(fontSitesCallBackData[i].url);
+    						}
+    				}
+    			}
+    			console.debug($scope.missingFontWebsites);
+    			showDialog();
+			});
+		}
+		$("#logo").find("*").not("g").not("svg").each(function(i,ob) { //get all elements in id logo but not the groups and svg
+			$.each(coloredElements,function(x,el){
+				var rawColor = $(ob).css(el).match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+				if(rawColor != null){
+					var color = {css:rawColor[0],r:rawColor[1],g:rawColor[2],b:rawColor[3]};
+					for (var i = 0; i < $scope.colorElements.length; i++) {
+						if($scope.colorElements[i].color.css == color.css){
+							$scope.colorElements[i].objects.push({ob:ob,el:el});
+							return true; //continue each
+						}
+					};
+					var objects = [];
+					objects.push({ob:ob,el:el});
+					$scope.colorElements.push({color:color,objects:objects});//create new for new color	
 				}
-			};
-			var objects = [];
-			objects.push(ob);
-			$scope.colorElements.push({color:color,objects:objects});//create new for new color
+			})
 		});
 
 		$scope.$apply();
@@ -46,36 +155,15 @@ var coloredElements = ["fill"]; //totdo get all style elements that color
 		  });
 
 	});
-	  $scope.update = function(){
-	  	$(".text" ).each(function(i,ob) {
-	  		console.debug($scope.textElements[i].text);
-	 		ob.textContent = $scope.textElements[i].text;
-	 	});
+	  $scope.updateText = function(set){
+	 		$.each(set.objects,function(i,ob) {
+	  			ob.textContent = set.text;
+	  	});
 	 }
 	 $scope.updateColor = function(set){
 	  	$.each(set.objects,function(i,ob) {
-	  		console.debug(ob);
-	  		$(ob).css("fill","rgb(" + set.color.r + "," + set.color.g + "," + set.color.b + ")");
-	  	})
-	 }
+	  		$(ob.ob).css(ob.el,"rgb(" + set.color.r + "," + set.color.g + "," + set.color.b + ")");
+	  	});
+	}
+});
 
-	});
-
-function hexToRgb(hex) {
-    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-        return r + r + g + g + b + b;
-    });
-
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
-}
-
-function rgbToHex(r, g, b) {
-    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
--- logotool.php
+++ logotool.php
@@ -7,12 +7,20 @@
 	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/angular_material/0.9.4/angular-material.min.css">
 	<link rel="stylesheet" href="font/font-awesome/css/font-awesome.min.css">
 	<script src="js/jquery.js" type="text/javascript"></script>
+	<script src="js/fontdetect.js" type="text/javascript"></script>
 	<style type="text/css">
 		svg{
 			margin:auto;
 			width:90vw;
-			max-width: 500px;
+			max-width: 90%;
 		}
+		@media screen and (min-width:600px) and (max-width: 960px) {
+    	#settings {
+        	margin-left:auto;
+        	margin-right: auto;
+    		}
+		}
+
 		.content{
 			margin:auto;
 			
@@ -80,99 +88,101 @@
 	</div>
 </md-toolbar>
 
-
-<div layout-sm="column" layout="row">
-	<div flex flex-order="2" flex-order-sm="1" layout="column">
-		<?php
-include "logo.php";
-?>
+<div layout-sm="column" layout-md="column" layout="row">
+	<div flex flex-order="2" flex-order-sm="1" flex-order-md="1" layout="column" >
+		<span layout-align="center start" id="logo" >
+			<?php
+				include "logo.php";
+			?>
+		</span>
 	</div>
-
-	<md-list flex-order="1" flex-order-sm="2">
-		<md-list-item>
-			<md-card>
-				<md-content>
-					<md-list>
-					  <md-subheader class="md-no-sticky">Text fields</md-subheader>
-					  <div ngbind="textElements" ng-repeat="field in textElements">
-							<md-list-item >
-								<div flex layout="column">
-										<md-input-container >
-										<label>Adjust text</label>
-										<input ng-model="field.text" ng-change="update()">
-									</md-input-container>
+	<div style="height:100vh">
+		<md-list flex-order="1" flex-order-sm="2" flex-order-md="2" id="settings">
+			<md-list-item>
+				<md-card>
+					<md-content>
+						<md-list>
+						  <md-subheader class="md-no-sticky">Text fields</md-subheader>
+						  <div ngbind="textElements" ng-repeat="field in textElements">
+								<md-list-item >
+									<div flex layout="column">
+											<md-input-container >
+											<label>Adjust text</label>
+											<input ng-model="field.text" ng-change="updateText(field)">
+										</md-input-container>
+									</div>
+								</md-list-item>
+								<md-divider ng-if="!$last"></md-divider>
 								</div>
-							</md-list-item>
-							<md-divider ng-if="!$last"></md-divider>
+						</md-list>
+					</md-content>
+				</md-card>
+			</md-list-item>
+			<md-list-item  md-colspan="1" md-rowspan="1" >
+				<md-card>
+					<md-content>
+						<md-list>
+						  <md-subheader class="md-no-sticky">Color fields</md-subheader>
+						  	<div ngbind="colorElements" ng-repeat="field in colorElements">
+							  	<md-list-item>
+									<div flex layout="column">
+										<div class="colorText">
+											Adjust color
+										</div>
+										<div class="color1 colorField" ngbind="field.color" style="background-color:rgb({{field.color.r}},{{field.color.g}},{{field.color.b}})">
+											&nbsp;
+										</div>
+									</div>
+								</md-list-item>
+								<md-card class="colorCard">
+									<md-card-content>
+										<div layout="row">
+											<md-slider flex min="0" max="255" ng-model="field.color.r" aria-label="red" id="red-slider" ng-change="updateColor(field)">
+											</md-slider>
+											<div flex="20"  layout layout-align="right center">
+												<md-input-container flex>
+													<label>Red</label>
+													<input ng-model="field.color.r">
+												</md-input-container>
+											</div>
+										</div>
+										<div layout="row">
+											<md-slider flex ng-model="field.color.g" min="0" max="255" aria-label="green" id="green-slider" ng-change="updateColor(field)">
+											</md-slider>
+											<div flex="20"  layout layout-align="right center">
+												<md-input-container flex>
+													<label>Green</label>
+													<input ng-model="field.color.g">
+												</md-input-container>
+											</div>
+										</div>
+										<div layout="row">
+											<md-slider flex ng-model="field.color.b" min="0" max="255" aria-label="blue" id="blue-slider" ng-change="updateColor(field)">
+											</md-slider>
+											<div flex="20"  layout layout-align="right center">
+												<md-input-container flex>
+													<label>Blue</label>
+													<input ng-model="field.color.b">
+												</md-input-container>
+											</div>
+										</div>
+									</md-card-content>
+								</md-card>
+								<md-divider ng-if="!$last"></md-divider>
 							</div>
-					</md-list>
-				</md-content>
-			</md-card>
-		</md-list-item>
-		<md-list-item  md-colspan="1" md-rowspan="1" >
-			<md-card>
-				<md-content>
-					<md-list>
-					  <md-subheader class="md-no-sticky">Color fields</md-subheader>
-					  	<div ngbind="colorElements" ng-repeat="field in colorElements">
-						  	<md-list-item>
-								<div flex layout="column">
-									<div class="colorText">
-										Adjust color
-									</div>
-									<div class="color1 colorField" ngbind="field.color" style="background-color:rgb({{field.color.r}},{{field.color.g}},{{field.color.b}})">
-										&nbsp;
-									</div>
-								</div>
-							</md-list-item>
-							<md-card class="colorCard">
-								<md-card-content>
-									<div layout="row">
-										<md-slider flex min="0" max="255" ng-model="field.color.r" aria-label="red" id="red-slider" ng-change="updateColor(field)">
-										</md-slider>
-										<div flex="20"  layout layout-align="right center">
-											<md-input-container flex>
-												<label>Red</label>
-												<input ng-model="field.color.r">
-											</md-input-container>
-										</div>
-									</div>
-									<div layout="row">
-										<md-slider flex ng-model="field.color.g" min="0" max="255" aria-label="green" id="green-slider" ng-change="updateColor(field)">
-										</md-slider>
-										<div flex="20"  layout layout-align="right center">
-											<md-input-container flex>
-												<label>Green</label>
-												<input ng-model="field.color.g">
-											</md-input-container>
-										</div>
-									</div>
-									<div layout="row">
-										<md-slider flex ng-model="field.color.b" min="0" max="255" aria-label="blue" id="blue-slider" ng-change="updateColor(field)">
-										</md-slider>
-										<div flex="20"  layout layout-align="right center">
-											<md-input-container flex>
-												<label>Blue</label>
-												<input ng-model="field.color.b">
-											</md-input-container>
-										</div>
-									</div>
-								</md-card-content>
-							</md-card>
-							<md-divider ng-if="!$last"></md-divider>
-						</div>
-					</md-list>
-				</md-content>
-			</md-card>
-		</md-list-item>
-		<md-list-item layout layout-align="center center">
-			<md-button class="md-fab" aria-label="FAB">
-				<i class="fa fa-download"></i>
-			</md-button>
-		</md-list-item>
-	</md-list>
-</div>
-<div id='test'  ng-bind-html="logo">{{logo}}</div>
+						</md-list>
+					</md-content>
+				</md-card>
+			</md-list-item>
+			<md-list-item layout layout-align="center center">
+				<md-button class="md-fab" aria-label="FAB">
+					<i class="fa fa-download"></i>
+				</md-button>
+			</md-list-item>
+		</md-list>
+	</div>
+	
+
 
 
 
